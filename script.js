528d0cb137be908f3817cfb7d416e1c444c81b35
Melona-Png <madisont.woodward@gmail.com>
2023-01-12 00:37:07 -0500

    removed pseudocode and added additional comments

--- script.js
+++ script.js
@@ -39,21 +39,4 @@ $(document).ready(function () {
   $("#14 .description").text(localStorage.getItem("14"));
   $("#15 .description").text(localStorage.getItem("15"));
   $("#16 .description").text(localStorage.getItem("16"));
-
- // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  // Apply styling to elements based on time block through the ID
-  // for loop to determine current time vs time blocks
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // localStorage.getItem(key) to collect saved data from line 47
-  // fetch and .textContent to grab data from each key and display to screen using forloop
 });
